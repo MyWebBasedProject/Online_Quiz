6f9bacc64138e820482d5db236fedff0696950ee
Mridul Rajbhar <mridulrajbhar12@gmail.com>
2022-01-21 10:47:06 +0530

    modified testing.py

--- onlineexam/testing.py
+++ onlineexam/testing.py
@@ -1,17 +1,26 @@
 import cv2
 import time
 import face_recognition
-import simple_websocket
 import dlib
+import threading
 import numpy as np
 import math
-from flask_socketio import send, emit
+from flask_socketio import emit
 from flask import render_template, request
 from onlineexam import app, socketio, backend
 
 cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+correct_person = [False]
+persons = 0
+mobiles = 0
+eye_direction = "center"
+face_direction = "center"
+face_count = 0
+canbreak = False
+
 
 classes_names = []
 with open("onlineexam\yolov3\coco.names", "r") as f:
@@ -21,26 +30,23 @@ net = cv2.dnn.readNet("onlineexam\yolov3\yolov3-spp.weights",
 layers_names = net.getLayerNames()
 output_layers = [layers_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-
-check_rate = 2
-correct_person = False
-
 imgMridul = face_recognition.load_image_file("onlineexam\images\Mridul.jpg")
 imgMridul = cv2.cvtColor(imgMridul, cv2.COLOR_BGR2RGB)
 refEncode = face_recognition.face_encodings(imgMridul)[0]
-face_count = 0
 
 # imgRdj = face_recognition.load_image_file("onlineexam\images\_rdj.jpg")
 # imgRdj = cv2.cvtColor(imgRdj, cv2.COLOR_BGR2RGB)
 # refEncode = face_recognition.face_encodings(imgRdj)[0]
 
 
-def compare_faces(frame):
-    if len(face_recognition.face_encodings(frame)) != 0:
-        encode = face_recognition.face_encodings(frame)[0]
-        return face_recognition.compare_faces([refEncode], encode)
-    else:
-        return [False]
+def check_correct_person(frame):
+    global correct_person
+    face_locations = face_recognition.face_locations(frame)
+    if len(face_locations) == 1:
+        face_encodes = face_recognition.face_encodings(
+            frame, face_locations)
+        correct_person = face_recognition.compare_faces(
+            refEncode, face_encodes)
 
 
 @app.route("/face_detect", methods=["POST"])
@@ -126,6 +132,7 @@ def get_slopes(pointA, pointB):
 
 
 def face_orientation(backendInstance, frame, landmarks):
+    global face_direction
     nose_region = nose_area([27, 28, 29, 30, 33], landmarks)
     # print(np.size(nose_region))
     m1 = get_slopes([landmarks.part(27).x, landmarks.part(27).y], [
@@ -138,23 +145,27 @@ def face_orientation(backendInstance, frame, landmarks):
         angle_d = round(math.degrees(angle_r))
         cv2.putText(frame, str(angle_d), (300, 100),
                     cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255), 3)
-        if angle_d <= 60 and angle_d > 0:
+        if angle_d <= 65 and angle_d > 0:
+            face_direction = "left side"
             cv2.putText(frame, "Left Side", (100, 100),
                         cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255), 3)
-            #backendInstance.insert_message('Face Left Side', frame)
+            # backendInstance.insert_message('Face Left Side', frame)
             return False
-        elif angle_d >= -60 and angle_d < 0:
+        elif angle_d >= -65 and angle_d < 0:
+            face_direction = "right side"
             cv2.putText(frame, "Right Side", (100, 100),
                         cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255), 3)
-            #backendInstance.insert_message('Face Right Side', frame)
+            # backendInstance.insert_message('Face Right Side', frame)
             return False
         else:
+            face_direction = "center side"
             cv2.putText(frame, "center", (100, 100),
                         cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255), 3)
             return True
 
 
 def gaze_calcualtion(backenInstance, frame, gray, landmarks):
+    global eye_direction
     left_eye = [36, 37, 38, 39, 40, 41]
     right_eye = [42, 43, 44, 45, 46, 47]
     left_l, center_l, right_l = gaze_detection(
@@ -166,106 +177,105 @@ def gaze_calcualtion(backenInstance, frame, gray, landmarks):
     right = right_l + right_r
 
     if left < right and left < center:
+        eye_direction = "left"
         cv2.putText(frame, "Eye: Left", (50, 200),
                     cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 3)
-        #backenInstance.insert_message('Looking Left', frame)
+        # backenInstance.insert_message('Looking Left', frame)
     elif right < left and right < center:
+        eye_direction = "right"
         cv2.putText(frame, "Eye: Right", (50, 200),
                     cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 255), 3)
-        #backenInstance.insert_message('Looking Right', frame)
+        # backenInstance.insert_message('Looking Right', frame)
     elif center < left and center < right:
+        eye_direction = "center"
         cv2.putText(frame, "Eye: Center", (50, 200),
                     cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 0), 3)
 
 
 def face_area(frame, x1, y1, x2, y2):
-    #cv2.putText(frame, str((y2 - y1) + (x2 - x1)), (300, 300), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,255), 3)
+    # cv2.putText(frame, str((y2 - y1) + (x2 - x1)), (300, 300), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,255), 3)
     return (y2 - y1)+(x2 - x1)
 
 
-correct_person = [False]
-
-
-@socketio.on('detect_correct_person')
-def detect_correct_person():
-    while True:
-        correct, frame = cap.read()
-        frame = cv2.flip(frame, 1)
-        # convert to gray image
-        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if face_count == 1:
-            correct_person = compare_faces(frame)
-
-        cv2.imshow("correct person", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-
-@socketio.on('check_persons')
 def detect_person_mobile():
+    global last_time, persons, mobiles, canbreak, correct_person
+    str_correct_person = True
     while True:
+        if correct_person[0]==True:
+            str_correct_person = True
+        else:
+            str_correct_person = False
         correct, frame = cap.read()  # normal image capture in RGB format
-        frame = cv2.flip(frame, 1)
-        # detecting image
-        # opencv works in BGR format so we r converting it.
-        blob = cv2.dnn.blobFromImage(
-            frame, 0.00392, (320, 320), (0, 0, 0), True, crop=False)
+        if correct:
+            frame = cv2.flip(frame, 1)
+            # detecting image
+            # opencv works in BGR format so we r converting it.
+            blob = cv2.dnn.blobFromImage(
+                frame, 0.00392, (320, 320), (0, 0, 0), True, crop=False)
 
-        net.setInput(blob)
-        outs = net.forward(output_layers)
+            net.setInput(blob)
+            outs = net.forward(output_layers)
 
-        height, width, channels = frame.shape
+            height, width, channels = frame.shape
 
-        confidences = []
-        boxes = []
-        class_ids = []
-        for out in outs:
-            for detection in out:
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                confidence = scores[class_id]
-                if confidence > 0.3 and (class_id == 0 or class_id == 67):
-                    center_x = int(detection[0] * width)
-                    center_y = int(detection[1] * height)
-                    w = int(detection[2]*width)
-                    h = int(detection[3]*height)
+            confidences = []
+            boxes = []
+            class_ids = []
+            for out in outs:
+                for detection in out:
+                    scores = detection[5:]
+                    class_id = np.argmax(scores)
+                    confidence = scores[class_id]
+                    if confidence > 0.3 and (class_id == 0 or class_id == 67):
+                        center_x = int(detection[0] * width)
+                        center_y = int(detection[1] * height)
+                        w = int(detection[2]*width)
+                        h = int(detection[3]*height)
 
-                    # Rectangle Coordinates
-                    x = int(center_x - w/2)
-                    y = int(center_y - h/2)
+                        # Rectangle Coordinates
+                        x = int(center_x - w/2)
+                        y = int(center_y - h/2)
 
-                    boxes.append([x, y, w, h])
-                    confidences.append(float(confidence))
-                    class_ids.append(class_id)
+                        boxes.append([x, y, w, h])
+                        confidences.append(float(confidence))
+                        class_ids.append(class_id)
 
-        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-        number_object_detected = len(boxes)
+            indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+            number_object_detected = len(boxes)
 
-        persons = 0
-        mobiles = 0
+            persons = 0
+            mobiles = 0
 
-        for i in range(number_object_detected):
-            if i in indexes:
-                if class_ids[i] == 0:
-                    persons += 1
-                x, y, w, h = boxes[i]
-                label = str(classes_names[class_ids[i]])
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-                cv2.putText(
-                    frame, label + ": " + str(class_ids[i]), (x, y+40), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+            for i in range(number_object_detected):
+                if i in indexes:
+                    if class_ids[i] == 0:
+                        persons += 1
+                    if class_ids[i] == 67:
+                        mobiles += 1
+                    x, y, w, h = boxes[i]
+                    label = str(classes_names[class_ids[i]])
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+                    cv2.putText(
+                        frame, label + ": " + str(class_ids[i]), (x, y+40), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
 
-        cv2.putText(frame, str(face_count), (50, 100),
-                    cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 255))
-        cv2.imshow("person mobile", frame)
+            cv2.putText(frame, str(face_count), (50, 100),
+                        cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 255))
+            cv2.imshow("person mobile", frame)
 
-        if face_count == 1:
-            correct_person = compare_faces(frame)
-            print(correct_person)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            if face_count == 1:
+                check_correct_person(frame)
+
+            socketio.emit('detect_person_mobile',
+                          (persons, mobiles, str_correct_person))
+        if cv2.waitKey(300) & canbreak == True:
             break
 
 
-@socketio.on('check_violation')
+# def emit_violation():
+#     emit('violation', (face_count, eye_direction, face_direction))
+
+
+# @socketio.on('check_violation')
 def violation():
     # Model
     last_time = 0
@@ -273,51 +283,74 @@ def violation():
 
     while True:
         correct, frame = cap.read()  # normal image capture in RGB format
-        frame = cv2.flip(frame, 1)
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to gray image
-        faces = detector(gray)  # apply detector on gray image
-        face_count = len(faces)
-        if face_count > 0:
-            for face in faces:  # for each face detected by detector
-                x1, y1 = face.left(), face.top()
-                x2, y2 = face.right(), face.bottom()
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                face_perimenter = face_area(frame, x1, y1, x2, y2)
+        if correct:
+            frame = cv2.flip(frame, 1)
+            # convert to gray image
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = detector(gray)  # apply detector on gray image
+            global face_count, persons, eye_direction, face_direction
+            face_count = len(faces)
+            if face_count > 0 and persons == 1:
+                for face in faces:  # for each face detected by detector
+                    x1, y1 = face.left(), face.top()
+                    x2, y2 = face.right(), face.bottom()
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    face_perimenter = face_area(frame, x1, y1, x2, y2)
 
-                landmarks = predictor(gray, face)
-                if face_count == 1:
-                    if face_perimenter > 400 and face_perimenter < 525 and time.time() > check_rate+last_time:
-                        last_time = time.time()
-                        cv2.putText(frame, str(correct_person), (50, 100),
-                                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255))
-                        if correct_person[0]:
-                            if face_orientation(backendInstance, frame, landmarks):
-                                gaze_calcualtion(
-                                    backendInstance, frame, gray, landmarks)
-                        else:
-                            cv2.putText(frame, "Wrong Person", (50, 100),
+                    landmarks = predictor(gray, face)
+
+                    if face_count == 1:
+                        if face_perimenter > 400 and face_perimenter < 525:
+                            last_time = time.time()
+                            cv2.putText(frame, str(correct_person), (50, 100),
                                         cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255))
-                    elif face_perimenter < 400:
-                        cv2.putText(frame, str("Face too far"), (100, 100),
-                                    cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 3)
-                    elif face_perimenter > 525:
-                        cv2.putText(frame, str("Face too close"), (100, 100),
-                                    cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 3)
-                elif face_count != 1 and time.time() > check_rate+last_time:
-                    last_time = time.time()
-                    cv2.putText(frame, str("Multiple Faces Detected: " + str(face_count)),
-                                (50, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
-                    #backendInstance.insert_message("Multiple Faces Detected",frame)
-        elif time.time() > last_time+check_rate:
-            last_time = time.time()
-            cv2.putText(frame, str("No Face Detected"), (50, 100),
-                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
-            #backendInstance.insert_message("No Face Detected", frame)
-        cv2.imshow("violation", frame)
+                            if correct_person[0]:
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+                                if face_orientation(backendInstance, frame, landmarks):
+                                    gaze_calcualtion(
+                                        backendInstance, frame, gray, landmarks)
+                            else:
+                                cv2.putText(frame, "Wrong Person", (50, 100),
+                                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255))
+                        elif face_perimenter < 400:
+                            cv2.putText(frame, str("Face too far"), (100, 100),
+                                        cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 3)
+                        elif face_perimenter > 525:
+                            cv2.putText(frame, str("Face too close"), (100, 100),
+                                        cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 3)
+                    elif face_count != 1:
+                        last_time = time.time()
+                        cv2.putText(frame, str("Multiple Faces Detected: " + str(face_count)),
+                                    (50, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                        # backendInstance.insert_message("Multiple Faces Detected",frame)
+            else:
+                cv2.putText(frame, str("No Face Detected"), (50, 100),
+                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 3)
+                # backendInstance.insert_message("No Face Detected", frame)
+
+            cv2.imshow("violation", frame)
+            socketio.emit('violation', (face_count,
+                          eye_direction, face_direction))
+        global canbreak
+        if cv2.waitKey(1) & canbreak == True:
             break
 
+
+@socketio.on('violation')
+def start_violation():
+    t1 = threading.Thread(target=detect_person_mobile)
+    t2 = threading.Thread(target=violation)
+
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+
+@socketio.on('close_camera')
+def close_camera():
     cap.release()
     cv2.destroyAllWindows()
+    global canbreak
+    canbreak = True

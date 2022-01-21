1a83f20e641431cdb2f3172064afe59a5448107f
Mridul Rajbhar <mridulrajbhar12@gmail.com>
2022-01-21 10:43:04 +0530

    modified init

--- onlineexam/__init__.py
+++ onlineexam/__init__.py
@@ -2,9 +2,8 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_mysqldb import MySQL
 
-
-app= Flask(__name__)
-app.config['SECRET_KEY']='secret!'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -12,5 +11,3 @@ app.config['MYSQL_DB'] = 'exam'
 
 socketio = SocketIO(app)
 mydb = MySQL(app)
-
-from onlineexam import webapp, testing

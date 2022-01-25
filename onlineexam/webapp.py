d9ce8fd83ff0a14e7a16368c47c8024c605eaf57
Mridul Rajbhar <mridulrajbhar12@gmail.com>
2022-01-25 12:17:55 +0530

    modified webapp

--- onlineexam/webapp.py
+++ onlineexam/webapp.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from onlineexam import app
 
 
@@ -6,3 +6,9 @@ from onlineexam import app
 def home():
     print("redirected")
     return render_template("index.html")
+
+
+@app.route("/face_detect", methods=["POST"])
+def faceDetect():
+    if request.method == "POST":
+        return render_template("face_detect.html")

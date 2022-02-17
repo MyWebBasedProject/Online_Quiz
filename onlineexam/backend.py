a84e8ced21d1fbf724f0d3da89118145bcd53edc
Mridul Dinesh Rajbhar <mridulrajbhar12@gmail.com>
2022-02-18 00:26:56 +0530

    updated backend.py

--- onlineexam/backend.py
+++ onlineexam/backend.py
@@ -1,30 +1,12 @@
 
 class BackendOperations:
-
-    def digital_to_binary(self, image_name):
-        with open(image_name, 'rb') as file:
-            binaryData = file.read()
-        return binaryData
-
-    def print_connection(self):
-        print("connection")
-
-    def insert_message(self, msg, image_name, mydb):
-
-        #cursor = MySQLdb.connect()
+    def insert_message(self, msg, image_path, mydb):
 
         cursor = mydb.cursor()
-        print("insert_meassage: cursor")
-        print(cursor)
         try:
-
-            # print("Record Insertertion Started")
-            # picture = self.digital_to_binary(image_name)
-
             cursor.execute(
-                'INSERT INTO temp_report(message, screenshot) VALUES (%s, %s)', (msg, image_name, ))
+                'INSERT INTO temp_report(message, screenshot) VALUES (%s, %s)', (msg, image_path, ))
             mydb.commit()
-            print("Record Inserted")
 
         except Exception as error:
             print("The error occured  is {}".format(error))
